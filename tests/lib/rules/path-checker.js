55e25465bac3d2d48901af998a00f7b2eb657708
Sidnev Victor <sidnevv@gmail.com>
2023-08-07 14:28:30 +0700

    add support for aliases

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -11,21 +11,41 @@
 const rule = require("../../../lib/rules/path-checker"),
   RuleTester = require("eslint").RuleTester;
 
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: "module" },
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: "C:\\Server\\domains\\react-ts-course\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "path checker",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: "C:\\Server\\domains\\react-ts-course\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [
+        {
+          message: "В рамках одного слайса все пути должны быть относительными",
+        },
+      ],
+      options: [{ alias: "@" }],
+    },
+    {
+      filename: "C:\\Server\\domains\\react-ts-course\\src\\entities\\Article",
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [
+        {
+          message: "В рамках одного слайса все пути должны быть относительными",
+        },
+      ],
     },
   ],
 });
